6822c0b718bb1ea0a19cbd205045684bf47d2759
prakashaditya13 <prakashaditya13011999+git@gmail.com>
2023-12-11 11:59:30 +0530

    Upd: Project added

--- src/constant/projectData.js
+++ src/constant/projectData.js
@@ -33,5 +33,20 @@ export const projectsData = [
       "fileName": "apartment_project_banner-7.png",
       "projectLink": "https://www.figma.com/proto/9WHhZBftxAtf1q7498piw2/Untitled?type=design&node-id=19-205&t=b0kAkfJp8wuGhGio-1&scaling=scale-down-width&page-id=0%3A1&starting-point-node-id=19%3A205",
       "projectName": "Apartment"
+    },
+    {
+      "fileName": "movie_8.png",
+      "projectLink": "https://mymovie.onrender.com/",
+      "projectName": "Moviex"
+    },
+    {
+      "fileName": "nitin_portfolio_9.png",
+      "projectLink": "https://www.figma.com/proto/tEzd2BgebkxBxkgCDuc5gI/Untitled?page-id=0%3A1&type=design&node-id=82-20&viewport=447%2C-550%2C0.16&scaling=scale-down-width&starting-point-node-id=82%3A20",
+      "projectName": "Nitin Portfolio"
+    },
+    {
+      "fileName": "appartment__app_10.png",
+      "projectLink": "https://www.figma.com/proto/9WHhZBftxAtf1q7498piw2/Untitled?type=design&node-id=217-3&t=gI9uoejQx8uh96dj-1&scaling=scale-down&page-id=217%3A2&starting-point-node-id=217%3A3",
+      "projectName": "Apartment App"
     }
   ]
